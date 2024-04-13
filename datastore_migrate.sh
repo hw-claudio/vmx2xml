0786488ac80677a3028c116d5444a14bbda0fafd
Claudio Fontana <cfontana@suse.de>
2024-04-13 11:07:14 +0200

    datastore_migrate.sh: small script improvement
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- datastore_migrate.sh
+++ datastore_migrate.sh
@@ -3,12 +3,12 @@
 # Example of a script that leverages vmx2xml.py to migrate
 # an entire datastore of .VMX VMs and referenced disks
 
-set -x
-
 DATASTORE1=/virt1-share-migration/datastore1
 DATASTORE2=/virt1-share-migration/datastore2
 
 for VMX in `find ${DATASTORE1} -name "*.vmx"` ; do
-    XML=`echo ${VMX} | sed "s,${DATASTORE1},${DATASTORE2},"`
+    XML=${VMX/${DATASTORE1}/${DATASTORE2}}
+    XML=${XML/%.vmx/.xml}
+    echo "datastore_migrate.sh: converting $VMX to $XML..."
     vmx2xml.py ${*} -d${DATASTORE1}=${DATASTORE2} -c -o ${XML}.xml -f ${VMX}
 done
