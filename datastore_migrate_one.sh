c5bc6218dd61af751528293a3b2b6197ff9df4d9
Claudio Fontana <cfontana@suse.de>
2024-04-15 22:44:14 +0200

    datastore_migrate_one.sh: add a simple script for migrating a single vmx
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

---
+++ datastore_migrate_one.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Example of a script that migrates a single VMX
+
+DATASTORE1=/virt1-share-migration/datastore1
+DATASTORE2=/virt1-share-migration/datastore-libvirt
+VMIMAGES=/virt1-share-migration/vmimages/floppies
+VMX=/virt1-share-migration/datastore1/w2k16esxi/w2k16esxi.vmx
+
+XML=${VMX/${DATASTORE1}/${DATASTORE2}}
+XML=${XML/%.vmx/.xml}
+echo "datastore_migrate_one.sh: converting $VMX to $XML..."
+vmx2xml.py -s ${VMIMAGES} -o ${XML} -f ${VMX} -d${DATASTORE1}=${DATASTORE2} ${*}
