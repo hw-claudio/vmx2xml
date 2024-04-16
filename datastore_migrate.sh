8ce9852b8803bf3e13226164aa669130a2c2207e
Claudio Fontana <cfontana@suse.de>
2024-04-16 14:02:25 +0200

    datastore_migrate: add a space
    so it's easier to copy paste into other commands
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- datastore_migrate.sh
+++ datastore_migrate.sh
@@ -10,6 +10,6 @@ VMIMAGES=/virt1-share-migration/vmimages/floppies
 for VMX in `find ${DATASTORE1} -name "*.vmx"` ; do
     XML=${VMX/${DATASTORE1}/${DATASTORE2}}
     XML=${XML/%.vmx/.xml}
-    echo "datastore_migrate.sh: converting $VMX to $XML..."
+    echo "datastore_migrate.sh: converting $VMX to $XML ..."
     vmx2xml.py -s ${VMIMAGES} -o ${XML} -f ${VMX} -d${DATASTORE1}=${DATASTORE2} ${*}
 done
--- datastore_migrate_one.sh
+++ datastore_migrate_one.sh
@@ -9,5 +9,5 @@ VMX=/virt1-share-migration/datastore1/w2k16esxi/w2k16esxi.vmx
 
 XML=${VMX/${DATASTORE1}/${DATASTORE2}}
 XML=${XML/%.vmx/.xml}
-echo "datastore_migrate_one.sh: converting $VMX to $XML..."
+echo "datastore_migrate_one.sh: converting $VMX to $XML ..."
 vmx2xml.py -s ${VMIMAGES} -o ${XML} -f ${VMX} -d${DATASTORE1}=${DATASTORE2} ${*}
