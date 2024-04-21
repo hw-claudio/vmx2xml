08cae5c3b2faaf6f1cf8e619cab756156a2d4593
Claudio Fontana <cfontana@suse.de>
2024-04-21 22:41:04 +0200

    datastore_migrate.sh: update examples
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- datastore_migrate.sh
+++ datastore_migrate.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
 
 # Example of a script that leverages vmx2xml.py to migrate
-# an entire datastore of .VMX VMs and referenced disks
+# an entire datastore of .VMX VMs and referenced disks.
+# See datastore_migrate_one.sh for more comments.
 
-DATASTORE1=/100G/datastore1/
-DATASTORE2=/100G/datastore-libvirt/
-VMIMAGES=/100G/vmimages/
+DS1=/100G/datastore1
+DS2=/100G/datastore-libvirt
+DS_MAP12=/vmfs/volumes/datastore1,${DS1}=${DS2}
 
-for VMX in `find ${DATASTORE1} -name "*.vmx"` ; do
-    XML=${VMX/${DATASTORE1}/${DATASTORE2}}
+DS3=/100G/datastore-independent1
+DS4=/100G/datastore-libvirt-independent/
+DS_MAP34=/vmfs/volumes/7e06e1f8-f272f9cd/datastore-volumes,${DS3}=${DS4}
+
+VMIMAGES=/vmimages,/100G/vmimages
+
+# In this simple loop we assume that we want to convert all VMX in DS1 into DS2.
+# For any external disk referenced by VMs in DS3, we want to put those disks in DS4.
+
+for VMX in `find ${DS1} -name "*.vmx"` ; do
+    XML=${VMX/${DS1}/${DS2}}
     XML=${XML/%.vmx/.xml}
     echo "datastore_migrate.sh: converting $VMX to $XML ..."
-    vmx2xml.py -s ${VMIMAGES} -o ${XML} -f ${VMX} -d${DATASTORE1}=${DATASTORE2} ${*}
+    vmx2xml.py -o ${XML} -f ${VMX} -d ${VMIMAGES} -d ${DS_MAP12} -d ${DS_MAP34} ${*}
 done
--- datastore_migrate_one.sh
+++ datastore_migrate_one.sh
@@ -8,24 +8,30 @@
 # This will be converted to the destination libvirt datastore (separated by equal: '='),
 # ie /100G/datastore-libvirt.
 #
-
-DS_MAP1=/vmfs/volumes/datastore1,/100G/datastore1=/100G/datastore-libvirt
+DS1=/100G/datastore1
+DS2=/100G/datastore-libvirt
+DS_MAP12=/vmfs/volumes/datastore1,${DS1}=${DS2}
 
 # In this example we have an external independent persistent disk of ~100G, without an OS,
 # that is used to store data.
 
-DS_MAP2=/vmfs/volumes/7e06e1f8-f272f9cd/datastore-volumes,/100G/datastore-independent1=/100G/datastore-libvirt-independent/
+DS3=/100G/datastore-independent1
+DS4=/100G/datastore-libvirt-independent/
+DS_MAP34=/vmfs/volumes/7e06e1f8-f272f9cd/datastore-volumes,${DS3}=${DS4}
 
 # Here we have a shared directory mapping that does not need any conversion, since it's just
-# floppies, maybe ISOs that can stay there.
+# floppies, ISOs etc that can stay there.
 
 VMIMAGES=/vmimages,/100G/vmimages
 
-# This is the VMX definition we want to convert
-VMX=/100G/datastore1/15sp6bios/15sp6bios.vmx
+# This is the VMX definition we want to convert. It is in DS1.
+VMX=${DS1}/15sp6bios/15sp6bios.vmx
+
+# We want it converted and copied into DS2.
+XML=${VMX/${DS1}/${DS2}}
 
 # This is the XML libvirt definition we want to convert to
 XML=/100G/datastore-libvirt/15sp6bios/15sp6bios.xml
 
 echo "datastore_migrate_one.sh: converting $VMX to $XML ..."
-vmx2xml.py -o ${XML} -f ${VMX} -d ${VMIMAGES} -d ${DS_MAP1} -d ${DS_MAP2} ${*}
+vmx2xml.py -o ${XML} -f ${VMX} -d ${VMIMAGES} -d ${DS_MAP12} -d ${DS_MAP34} ${*}
