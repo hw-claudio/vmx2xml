c2b34e0b7fda782be3131cedb83b9030fe4231dd
Claudio Fontana <cfontana@suse.de>
2024-05-05 20:22:24 +0200

    datastore_migrate: adapt to the new setup in the lab
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- datastore_migrate.sh
+++ datastore_migrate.sh
@@ -4,15 +4,15 @@
 # an entire datastore of .VMX VMs and referenced disks.
 # See datastore_migrate_one.sh for more comments.
 
-DS1=/100G/datastore1
-DS2=/100G/datastore-libvirt
+DS1=/153-RAID0/datastore1
+DS2=/154-RAID0/datastore-libvirt
 DS_MAP12=/vmfs/volumes/datastore1,${DS1}=${DS2}
 
-DS3=/100G/datastore-independent1
-DS4=/100G/datastore-libvirt-independent/
+DS3=/153-RAID0/datastore-independent1
+DS4=/154-RAID0/datastore-libvirt-independent
 DS_MAP34=/vmfs/volumes/7e06e1f8-f272f9cd/datastore-volumes,${DS3}=${DS4}
 
-VMIMAGES=/vmimages,/100G/vmimages
+VMIMAGES=/vmimages,/153-RAID0/vmimages
 
 # In this simple loop we assume that we want to convert all VMX in DS1 into DS2.
 # For any external disk referenced by VMs in DS3, we want to put those disks in DS4.
--- datastore_migrate_one.sh
+++ datastore_migrate_one.sh
@@ -3,26 +3,26 @@
 
 # Here we map the VMWare Datastore /vmfs/volumes/datastore1 as appears in the VMX files
 # to the directory it will be mounted on the conversion host (separated by comma: ','),
-# /100G/datastore1.
+# /153-RAID0/datastore1
 #
 # This will be converted to the destination libvirt datastore (separated by equal: '='),
-# ie /100G/datastore-libvirt.
+# ie /154-RAID0/datastore-libvirt
 #
-DS1=/100G/datastore1
-DS2=/100G/datastore-libvirt
+DS1=/153-RAID0/datastore1
+DS2=/154-RAID0/datastore-libvirt
 DS_MAP12=/vmfs/volumes/datastore1,${DS1}=${DS2}
 
 # In this example we have an external independent persistent disk of ~100G, without an OS,
 # that is used to store data.
 
-DS3=/100G/datastore-independent1
-DS4=/100G/datastore-libvirt-independent/
+DS3=/153-RAID0/datastore-independent1
+DS4=/154-RAID0/datastore-libvirt-independent
 DS_MAP34=/vmfs/volumes/7e06e1f8-f272f9cd/datastore-volumes,${DS3}=${DS4}
 
 # Here we have a shared directory mapping that does not need any conversion, since it's just
 # floppies, ISOs etc that can stay there.
 
-VMIMAGES=/vmimages,/100G/vmimages
+VMIMAGES=/vmimages,/153-RAID0/vmimages
 
 # This is the VMX definition we want to convert. It is in DS1.
 VMX=${DS1}/15sp6bios/15sp6bios.vmx
@@ -31,7 +31,7 @@ VMX=${DS1}/15sp6bios/15sp6bios.vmx
 XML=${VMX/${DS1}/${DS2}}
 
 # This is the XML libvirt definition we want to convert to
-XML=/100G/datastore-libvirt/15sp6bios/15sp6bios.xml
+XML=/154-RAID0/datastore-libvirt/15sp6bios/15sp6bios.xml
 
 echo "datastore_migrate_one.sh: converting $VMX to $XML ..."
 vmx2xml.py -o ${XML} -f ${VMX} -d ${VMIMAGES} -d ${DS_MAP12} -d ${DS_MAP34} ${*}
