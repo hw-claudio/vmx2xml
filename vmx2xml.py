19407a6daf1c96a069533bf076a45c6574f5be6d
Claudio Fontana <cfontana@suse.de>
2024-05-09 17:58:53 +0200

    vmx2xml: add option -r, --raw to generate RAW images instead of QCOW2
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- vmx2xml.py
+++ vmx2xml.py
@@ -28,10 +28,6 @@ import time
 log: logging.Logger = logging.getLogger(__name__)
 program_version: str = "0.1"
 
-def numa_restrict_cmd(numa_node: int) -> list:
-    return [ "numactl", "-m", str(numa_node), "-N", str(numa_node), "--" ]
-
-
 def log_disable_nl() -> None:
     global log
     handler: logging.StreamHandler = log.handlers[0]
@@ -67,41 +63,50 @@ def virt_inspector(path: str) -> dict:
     return osd
 
 
-def v2v_img_convert(vmdk: str, qcow: str, trace_cmd: bool, numa_node: int) -> None:
-    dirname: str = os.path.dirname(qcow)
+def numa_restrict_cmd(numa_node: int) -> list:
+    return [ "numactl", "-m", str(numa_node), "-N", str(numa_node), "--" ]
+
+
+def file_ext(raw: bool) -> str:
+    return "raw" if (raw) else "qcow2"
+
+
+def v2v_img_convert(from_file: str, to_file: str, trace_cmd: bool, numa_node: int, raw: bool) -> None:
+    to_file_ext: str = file_ext(raw)
+    dirname: str = os.path.dirname(to_file)
     args: list = []
     if (trace_cmd):
         args.extend([ "trace-cmd", "record", "-o", "/tmp/trace-v2v.dat", "-e", "sched" ])
     if (numa_node >= 0):
         args.extend(numa_restrict_cmd(numa_node))
-    args.extend([ "virt-v2v", "--root=first", "-i", "disk", "-o", "disk", "-of", "qcow2", "-os", dirname ])
+    args.extend([ "virt-v2v", "--root=first", "-i", "disk", "-o", "disk", "-of", to_file_ext, "-os", dirname ])
 
     if (log.level > logging.WARNING):
         args.append("--quiet")
     if (log.level <= logging.DEBUG):
         args.extend(["--verbose", "-x"])
-    args.append(vmdk)
+    args.append(from_file)
 
     log.debug("%s", args)
     p = subprocess.run(args, stdout=subprocess.DEVNULL, encoding='utf-8')
 
     if (p.returncode == 0):
-        log.info("virt-v2v: reports success converting disk %s", vmdk)
+        log.info("virt-v2v: reports success converting disk %s", from_file)
     else:
-        log.warning("virt-v2v: reports failure converting disk %s", vmdk)
+        log.warning("virt-v2v: reports failure converting disk %s", from_file)
 
     # Now rename to the name we want
-    srcnames: list = glob.glob(qcow[0:-len(".qcow2")] + "-sd*")
+    srcnames: list = glob.glob(to_file[0:-len(f".{to_file_ext}")] + "-sd*")
     if (len(srcnames) != 1):
-        log.critical("could not find the generated disk %s", qcow)
+        log.critical("could not find the generated disk %s", to_file)
         sys.exit(1)
-    os.rename(srcnames[0], qcow)
+    os.rename(srcnames[0], to_file)
 
 
 # there is no annotation for Tempfile, so return type is unknown
-def qemu_img_create_overlay(vmdk: str):
+def qemu_img_create_overlay(from_file: str):
     tmp = tempfile.NamedTemporaryFile()
-    args: list = ["qemu-img", "create", "-b", vmdk, '-F', 'vmdk', '-f', 'qcow2']
+    args: list = ["qemu-img", "create", "-b", from_file, '-F', 'vmdk', '-f', 'qcow2']
     if (log.level > logging.DEBUG):
         args.append("-q")
     args.append(tmp.name)
@@ -110,34 +115,36 @@ def qemu_img_create_overlay(vmdk: str):
     return tmp
 
 
-def qemu_img_create(qcow: str, vsize: int) -> None:
-    args: list = ["qemu-img", "create", "-f", "qcow2" ]
+def qemu_img_create(to_file: str, vsize: int, raw: bool) -> None:
+    to_file_ext: str = file_ext(raw)
+    args: list = ["qemu-img", "create", "-f", to_file_ext]
     if (log.level > logging.DEBUG):
         args.append("-q")
-    args.extend([qcow, str(vsize)])
+    args.extend([to_file, str(vsize)])
     log.debug("%s", args)
     p = subprocess.run(args, stdout=sys.stderr, check=True)
 
 
-def qemu_img_copy(vmdk: str, qcow: str, trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int) -> None:
+def qemu_img_copy(from_file: str, to_file: str, trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int, raw: bool) -> None:
+    to_file_ext: str = file_ext(raw)
     args: list = []
     if (trace_cmd):
         args.extend([ "trace-cmd", "record", "-o", "/tmp/trace-qemu-img.dat", "-e", "sched" ])
     if (numa_node >= 0):
         args.extend(numa_restrict_cmd(numa_node))
-    args.extend([ "qemu-img", "convert", "-O", "qcow2", "-t", cache_mode, "-T", cache_mode ])
+    args.extend([ "qemu-img", "convert", "-O", to_file_ext, "-t", cache_mode, "-T", cache_mode ])
     if (parallel > 0):
         args.extend([ "-m", str(parallel) ])
     if (log.getEffectiveLevel() <= logging.WARNING):
         args.append("-p")
-    args.extend([vmdk, qcow])
+    args.extend([from_file, to_file])
 
     log.debug("%s", args)
     p = subprocess.run(args, check=True)
 
 
-def qemu_img_info(vmdk: str) -> int:
-    args: list = ["qemu-img", "info", "-U", vmdk ]
+def qemu_img_info(from_file: str) -> int:
+    args: list = ["qemu-img", "info", "-U", from_file]
 
     log.debug("%s", args)
     p = subprocess.Popen(args, stdout=subprocess.PIPE, encoding='utf-8')
@@ -153,14 +160,14 @@ def qemu_img_info(vmdk: str) -> int:
     return int(vsize_m.group(1))
 
 
-def qemu_img_convert(sourcepath: str, targetpath: str, adjust: bool, trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int) -> None:
+def qemu_img_convert(sourcepath: str, targetpath: str, adjust: bool, trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int, raw: bool) -> None:
     src: str = sourcepath
     if (adjust):
         tmp = qemu_img_create_overlay(sourcepath)
         guestfs_adjust(tmp.name, False)
         src = tmp.name
 
-    qemu_img_copy(src, targetpath, trace_cmd, cache_mode, numa_node, parallel)
+    qemu_img_copy(src, targetpath, trace_cmd, cache_mode, numa_node, parallel, raw)
     if (adjust):
         tmp.close()
 
@@ -169,9 +176,11 @@ def qemu_kill_nbd(pid: int) -> None:
     os.kill(pid, 15)
 
 
-def qemu_nbd_create(s: str, overlay: bool, cache_mode: str) -> tuple:
+def qemu_nbd_create(s: str, overlay: bool, cache_mode: str, raw: bool) -> tuple:
     tmp = tempfile.NamedTemporaryFile(delete=False)
     args: list = [ "qemu-nbd", f"--cache={cache_mode}", "-t", "--shared=0", "--discard=unmap", "--socket", tmp.name ]
+    if (raw):
+        args.extend(['-f', 'raw'])
     if (overlay):
         args.append("-s")
     args.append(s)
@@ -204,13 +213,13 @@ def qemu_nbd_copy(sin: str, sout: str, trace_cmd: bool, numa_node: int, parallel
     p = subprocess.run(args, check=True)
 
 
-def qemu_nbd_convert(sourcepath: str, targetpath: str, adjust: bool, trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int) -> None:
-    (sin, pidin) = qemu_nbd_create(sourcepath, adjust, cache_mode)
+def qemu_nbd_convert(sourcepath: str, targetpath: str, adjust: bool, trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int, raw: bool) -> None:
+    (sin, pidin) = qemu_nbd_create(sourcepath, adjust, cache_mode, False)
     if (adjust):
         guestfs_adjust(sin.name, True)
     vsize: int = qemu_img_info(sourcepath)
-    qemu_img_create(targetpath, vsize)
-    (sout, pidout) = qemu_nbd_create(targetpath, False, cache_mode)
+    qemu_img_create(targetpath, vsize, raw)
+    (sout, pidout) = qemu_nbd_create(targetpath, False, cache_mode, raw)
     qemu_nbd_copy(sin.name, sout.name, trace_cmd, numa_node, parallel)
     sin.close()
     sout.close()
@@ -236,7 +245,7 @@ def parse_boolean(s: str) -> bool:
 
 
 # parse a Reference to a filename in the VMX
-def parse_filename_ref(s: str, datastores: dict, translate_qcow2: bool) -> list:
+def parse_filename_ref(s: str, datastores: dict, translate_disk: bool, raw: bool) -> list:
     # an empty string is valid, not really present.
     if (not s):
         return [None, None]
@@ -282,9 +291,10 @@ def parse_filename_ref(s: str, datastores: dict, translate_qcow2: bool) -> list:
         log.critical("\n%s NOT FOUND, datastores %s", basename, datastores)
         sys.exit(1)
 
-    if (translate_qcow2):
-        (match, is_qcow) = re.subn(r"\.vmdk$", ".qcow2", paths[1], count=1, flags=re.IGNORECASE)
-        if (is_qcow == 1):
+    if (translate_disk):
+        to_file_ext: str = file_ext(raw)
+        (match, is_vmdk) = re.subn(r"\.vmdk$", f".{to_file_ext}", paths[1], count=1, flags=re.IGNORECASE)
+        if (is_vmdk == 1):
             paths[1] = match
 
     log.info("%s", paths)
@@ -386,7 +396,7 @@ def find_disk_controllers(d: defaultdict, interface: str) -> dict:
     return controllers
 
 
-def find_disks(d: defaultdict, datastores: dict, interface: str, controllers: dict, qcow_mode: int) -> list:
+def find_disks(d: defaultdict, datastores: dict, interface: str, controllers: dict, disk_mode: int, raw: bool) -> list:
     disks: list = []
     for x in range(4):
         if (x not in controllers) and (interface != "ide"):  # IDE does not show explicit controllers entries
@@ -404,12 +414,12 @@ def find_disks(d: defaultdict, datastores: dict, interface: str, controllers: di
             }
             t: str = d[f"{interface}{x}:{y}.devicetype"].lower()
             disk["device"] = "cdrom" if ("cdrom" in t) else "disk"
-            disk["path"] = parse_filename_ref(d[f"{interface}{x}:{y}.filename"], datastores, qcow_mode >= 1)
+            disk["path"] = parse_filename_ref(d[f"{interface}{x}:{y}.filename"], datastores, disk_mode >= 1, raw)
             #disk["driver"] = "block" if (disk["path"].startswith("/dev/")) else "file"
             disk["driver"] = "file"
             # XXX we never use the actual libvirt/qemu default, writeback?
             disk["cache"] = "writethrough" if (parse_boolean(d[f"{interface}{x}:{y}.writethrough"])) else "none"
-            if (all(disk["path"]) and qcow_mode >= 2 and disk["path"][0].endswith(".vmdk")):
+            if (all(disk["path"]) and disk_mode >= 2 and disk["path"][0].endswith(".vmdk")):
                 disk["os"] = virt_inspector(disk["path"][0])
             disks.append(disk)
     return disks
@@ -521,28 +531,28 @@ def guestfs_adjust(path: str, nbd: bool) -> bool:
     return True
 
 
-def convert_path(sourcepath: str, targetpath: str, qcow_mode: int, datastores: dict, use_v2v: int, osd: dict,
-                 trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int, skip_adjust: bool) -> str:
+def convert_path(sourcepath: str, targetpath: str, disk_mode: int, datastores: dict, use_v2v: int, osd: dict,
+                 trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int, skip_adjust: bool, raw: bool) -> str:
     os.makedirs(os.path.dirname(targetpath), exist_ok=True)
-    if (qcow_mode <= 1):
+    if (disk_mode <= 1):
         # we need to create a pseudo disk for the virt install command to succeed
         if (not os.path.exists(targetpath)):
             open(targetpath, 'a').close()
         return targetpath
 
     # CONVERSION / MOVE asked
-    assert(qcow_mode >= 2)
-    if (targetpath.endswith(".qcow2")):
+    assert(disk_mode >= 2)
+    if (sourcepath.endswith(".vmdk")):
         has_os: bool = True if osd["name"] else False
         if (use_v2v == 1):
             if (has_os and not skip_adjust):
-                v2v_img_convert(sourcepath, targetpath, trace_cmd, numa_node)
+                v2v_img_convert(sourcepath, targetpath, trace_cmd, numa_node, raw)
             else:
-                qemu_nbd_convert(sourcepath, targetpath, False, trace_cmd, cache_mode, numa_node, parallel)
+                qemu_nbd_convert(sourcepath, targetpath, False, trace_cmd, cache_mode, numa_node, parallel, raw)
         elif (use_v2v == 0):
-            qemu_img_convert(sourcepath, targetpath, has_os and not skip_adjust, trace_cmd, cache_mode, numa_node, parallel)
+            qemu_img_convert(sourcepath, targetpath, has_os and not skip_adjust, trace_cmd, cache_mode, numa_node, parallel, raw)
         elif (use_v2v == -1):
-            qemu_nbd_convert(sourcepath, targetpath, has_os and not skip_adjust, trace_cmd, cache_mode, numa_node, parallel)
+            qemu_nbd_convert(sourcepath, targetpath, has_os and not skip_adjust, trace_cmd, cache_mode, numa_node, parallel, raw)
         else:
             assert(0) # unhandled use_v2v value
 
@@ -554,15 +564,15 @@ def convert_path(sourcepath: str, targetpath: str, qcow_mode: int, datastores: d
         except:
             log.info("could not compare to %s, assume we need to copy.", targetpath)
 
-        log.info("copy to non-QCOW target %s", targetpath)
+        log.info("copying non-VMDK disk %s", targetpath)
         # use copy2 so we try to preserve modification time.
         shutil.copy2(sourcepath, targetpath)
 
     return targetpath
 
 
-def virt_install(vinst_version: float, qcow_mode: int, datastores: dict, use_v2v: int, fidelity: bool,
-                 trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int, skip_adjust: bool, skip_extra: bool,
+def virt_install(vinst_version: float, disk_mode: int, datastores: dict, use_v2v: int, fidelity: bool,
+                 trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int, skip_adjust: bool, skip_extra: bool, raw: bool,
                  xml_name: str, vmx_name: str,
                  name: str, memory: int,
                  cpu: dict,
@@ -677,9 +687,9 @@ def virt_install(vinst_version: float, qcow_mode: int, datastores: dict, use_v2v
             log.info("skipping extra non-OS disk %s", paths[0])
             continue
         start_time: float = time.perf_counter()
-        path = convert_path(paths[0], paths[1], qcow_mode, datastores, use_v2v, disk["os"],
-                            trace_cmd, cache_mode, numa_node, parallel, skip_adjust)
-        if (qcow_mode >= 2):
+        path = convert_path(paths[0], paths[1], disk_mode, datastores, use_v2v, disk["os"],
+                            trace_cmd, cache_mode, numa_node, parallel, skip_adjust, raw)
+        if (disk_mode >= 2):
             end_time: float = time.perf_counter();
             elapsed: float = end_time - start_time
             if (elapsed > 0.0):
@@ -792,7 +802,7 @@ def detect_guestfs_adjust_version() -> float:
     return v
 
 
-def detect_qemu_img():
+def detect_qemu_img() -> None:
     args: list = [ "qemu-img", "--version" ]
     log.debug("%s", args)
     try:
@@ -844,7 +854,7 @@ def get_options(argc: int, argv: list) -> tuple:
     parser.add_argument('-o', '--output-xml', action='store', help='output libvirt XML file', required=True)
     parser.add_argument('-f', '--filename', metavar="VMXFILE", action='store', required=True,
                         help='the VMX description file to be converted. Its directory is added to input datastores')
-    parser.add_argument('-t', '--translate-qcow2', action='store_true', help='translate path references from .vmdk to .qcow2')
+    parser.add_argument('-t', '--translate-disks', action='store_true', help='translate path references from .vmdk to .qcow2 or .raw')
     parser.add_argument('-c', '--convert-disks', action='store_true', help='convert and move disk contents across datastores (implies -t)')
     parser.add_argument('-O', '--overwrite', action='store_true', help='run even when the output xml already exists (overwrite)')
     parser.add_argument('-x', '--experimental', action='store_true', help='use experimental old conversion method (qemu-img)')
@@ -858,6 +868,7 @@ def get_options(argc: int, argv: list) -> tuple:
     parser.add_argument('-p', '--parallel', action='store', type=int, default=-1, help='specify threads/connections/coroutines')
     parser.add_argument('-a', '--skip-adjust', action='store_true', help='skip guest adjustments to run on KVM')
     parser.add_argument('-X', '--skip-extra', action='store_true', help='skip extra non-OS VMDK/qcow2 disks')
+    parser.add_argument('-r', '--raw', action='store_true', help='generate RAW disk images instead of QCOW2')
 
     args: argparse.Namespace = parser.parse_args()
     if (args.experimental and args.experimental2):
@@ -910,13 +921,13 @@ def get_options(argc: int, argv: list) -> tuple:
                 targetpath = sourcepath
             datastores[ref] = (sourcepath, targetpath)
 
-    qcow_mode: int = 2 if (args.convert_disks) else 1 if (args.translate_qcow2) else 0
+    disk_mode: int = 2 if (args.convert_disks) else 1 if (args.translate_disks) else 0
     overwrite: bool = args.overwrite
 
-    log.debug("[OPTIONS] vmx_name=%s xml_name=%s qcow_mode:%s datastores:%s usev2v:%s overwrite:%s fidelity:%s "
-              "trace_cmd:%s cache_mode:%s numa_node:%s parallel:%s skip_adjust:%s skip_extra:%s",
-              vmx_name, xml_name, qcow_mode, datastores, use_v2v, overwrite, fidelity,
-              trace_cmd, cache_mode, numa_node, parallel, args.skip_adjust, args.skip_extra)
+    log.debug("[OPTIONS] vmx_name=%s xml_name=%s disk_mode:%s datastores:%s usev2v:%s overwrite:%s fidelity:%s "
+              "trace_cmd:%s cache_mode:%s numa_node:%s parallel:%s skip_adjust:%s skip_extra:%s raw:%s",
+              vmx_name, xml_name, disk_mode, datastores, use_v2v, overwrite, fidelity,
+              trace_cmd, cache_mode, numa_node, parallel, args.skip_adjust, args.skip_extra, args.raw)
 
     if (os.path.exists(xml_name)):
         if (not overwrite):
@@ -924,13 +935,13 @@ def get_options(argc: int, argv: list) -> tuple:
             sys.exit(0)
         log.warning("%s exists, overwriting", xml_name)
 
-    return (vmx_name, xml_name, qcow_mode, datastores, use_v2v, fidelity,
-            trace_cmd, cache_mode, numa_node, parallel, args.skip_adjust, args.skip_extra)
+    return (vmx_name, xml_name, disk_mode, datastores, use_v2v, fidelity,
+            trace_cmd, cache_mode, numa_node, parallel, args.skip_adjust, args.skip_extra, args.raw)
 
 
 def main(argc: int, argv: list) -> int:
-    (vmx_name, xml_name, qcow_mode, datastores, use_v2v, fidelity,
-     trace_cmd, cache_mode, numa_node, parallel, skip_adjust, skip_extra) = get_options(argc, argv)
+    (vmx_name, xml_name, disk_mode, datastores, use_v2v, fidelity,
+     trace_cmd, cache_mode, numa_node, parallel, skip_adjust, skip_extra, raw) = get_options(argc, argv)
 
     vinst_version: float = detect_vinst_version()
     adjust_version: float = detect_guestfs_adjust_version()
@@ -992,7 +1003,7 @@ def main(argc: int, argv: list) -> int:
             else:
                 uefi += ",firmware.feature0.name=secure-boot,firmware.feature0.enabled=no"
 
-    nvram: list = parse_filename_ref(d["nvram"], datastores, (qcow_mode >= 1))
+    nvram: list = parse_filename_ref(d["nvram"], datastores, (disk_mode >= 1), raw)
     if (uefi):
         log.debug("[UEFI] %s", uefi)
 
@@ -1016,11 +1027,11 @@ def main(argc: int, argv: list) -> int:
     disks: list = []
     for interface in disk_ctrls:
         disk_ctrls[interface] = find_disk_controllers(d, interface)
-        disks.extend(find_disks(d, datastores, interface, disk_ctrls[interface], qcow_mode))
+        disks.extend(find_disks(d, datastores, interface, disk_ctrls[interface], disk_mode, raw))
 
     floppys: list = [ [None, None], [None, None] ]
     for i in range(2):
-        floppys[i] = parse_filename_ref(d[f"floppy{i}.filename"], datastores, qcow_mode >= 1)
+        floppys[i] = parse_filename_ref(d[f"floppy{i}.filename"], datastores, disk_mode >= 1, raw)
 
     eths: list = find_eths(d, "ethernet")
 
@@ -1030,8 +1041,8 @@ def main(argc: int, argv: list) -> int:
     log.debug("%s", eths)
 
     # run virt-install to generate the xml
-    virt_install(vinst_version, qcow_mode, datastores, use_v2v, fidelity,
-                 trace_cmd, cache_mode, numa_node, parallel, skip_adjust, skip_extra,
+    virt_install(vinst_version, disk_mode, datastores, use_v2v, fidelity,
+                 trace_cmd, cache_mode, numa_node, parallel, skip_adjust, skip_extra, raw,
                  xml_name, vmx_name,
                  name, memory,
                  cpu,
