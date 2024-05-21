c72929f927525705e4cbbf6e0361e0617577de8f
Claudio Fontana <cfontana@suse.de>
2024-05-21 11:37:56 +0200

    install.sh: add a few new files to the install command
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 FILES="vmx2xml.py vmx2xml/ adjust_guestfs.py testboot_xml.py \
-datastore_migrate_one.sh datastore_migrate.sh \
-datastore_migrate_test_one.sh"
+datastore_migrate_one.sh datastore_migrate.sh net-isolated.xml \
+datastore_migrate_test_one.sh datastore_migrate_win.sh"
 
 set -x
 scp -r ${FILES} ${1}:/usr/local/bin/
