91684748269a71c605fada6541bf69fa8015f498
Claudio Fontana <cfontana@suse.de>
2024-05-31 14:10:41 +0200

    install.sh: update with the new files
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 FILES="vmx2xml.py vmx2xml/ adjust_guestfs.py testboot_xml.py \
 datastore_migrate_one.sh datastore_migrate.sh net-isolated.xml \
 datastore_migrate_test_one.sh datastore_migrate_win.sh \
-demo.py demo_testboot_xml.sh datastore_find_external_disks.sh art/"
+demo.py demo_testboot_xml.sh art/ \
+datastore_find_external_disks.sh datastore_find_networks.sh"
 
 set -x
 scp -r ${FILES} ${1}:/usr/local/bin/
