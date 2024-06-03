e0eb0cc78620af1ea7bd9cb5463e80e027a00e32
Claudio Fontana <cfontana@suse.de>
2024-06-03 10:15:28 +0200

    demo.py: reduce external disks and networks height
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- demo.py
+++ demo.py
@@ -715,7 +715,7 @@ def external_button_clicked(widget: Gtk.Widget):
     global external_window
     external_window.popup()
     external_window.show_all()
-    external_window.set_size_request(800, 320)
+    external_window.set_size_request(800, 256)
 
 
 def external_button_init() -> Gtk.MenuButton:
@@ -752,7 +752,7 @@ def networks_button_clicked(widget: Gtk.Widget):
     global networks_window
     networks_window.popup()
     networks_window.show_all()
-    networks_window.set_size_request(800, 320)
+    networks_window.set_size_request(800, 256)
 
 
 def networks_button_init() -> Gtk.MenuButton:
