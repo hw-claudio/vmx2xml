09d21c3317adb91ba7a801460a40435066c3a7f3
Claudio Fontana <cfontana@suse.de>
2024-06-07 17:16:01 +0200

    install.sh: small improvements
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- misc/install.sh
+++ misc/install.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # to be called from the previous directory
 
+DESTHOST=${1}
+DESTDIR=/usr/local/bin
 FILES="vmx2xml.py vmx2xml/ adjust_guestfs.py testboot_xml.py \
 datastore_migrate_one.sh datastore_migrate.sh \
 datastore_migrate_test_one.sh datastore_migrate_win.sh \
@@ -9,4 +11,5 @@ datastore_find_external_disks.sh datastore_find_networks.sh \
 misc/ "
 
 set -x
-scp -r ${FILES} ${1}:/usr/local/bin/
+scp -r ${FILES} ${DESTHOST}:${DESTDIR}/
+scp art/gtk.css ${DESTHOST}:/root/.config/gtk-3.0/gtk.css
