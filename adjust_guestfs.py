64129aff3d4129176aa6aa4880f6eb4c129327b1
Claudio Fontana <cfontana@suse.de>
2024-06-18 16:42:21 +0200

    LICENSE: add GPLv2 or later
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- adjust_guestfs.py
+++ adjust_guestfs.py
@@ -3,6 +3,20 @@
 # Copyright (c) 2024 SUSE LLC
 # Written by Claudio Fontana <claudio.fontana@suse.com>
 #
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 # This tool adjust the guest to run on KVM.
 # It is a simplified version of the virt-v2v convert_linux and convert_windows
 
