hw-claudio/vmx2xml

commit 2bb6313de1507b8369c9d3851a14ea406025a825

vmx2xml.py: refactor writing of XML to make --adjust-networks effective
we need to first get the XML,
then from the xml extract the guest mac addresses,
then convert the actual disks (since we need mac addresses for the adjustments),
and finally write the XML to the output file.

adjust: adapt to supply the new "-m" arguments to adjust_guestfs.py,
    and add a new macs list parameter to adjust_guestfs().

img: adapt to supply the new macs list parameter to adjust_guestfs.
testboot_xml.py: adapt to supply the new macs list parameter to adjust_guestfs.

Signed-off-by: Claudio Fontana <cfontana@suse.de>

vmx2xml.py
@@ -40,7 +40,7 @@ from vmx2xml_mod.adjust import adjust_guestfs_detect_version
 from vmx2xml_mod.inspector import inspector_detect_version, inspector_inspect
 from vmx2xml_mod.img import img_qemu_nbd_convert, img_qemu_convert, img_v2v_convert, img_file_ext
 from vmx2xml_mod.stopwatch import stopwatch_start, stopwatch_elapsed
-from vmx2xml_mod.runcmd import runcmd_detectv
+from vmx2xml_mod.runcmd import runcmd, runcmd_detectv
 
 program_version: str = "0.1"
 
@@ -352,16 +352,24 @@ def find_eths(d: defaultdict, interface: str, networks: dict, sandbox: str) -> l
 #     return translate(translator, s);
 
 
-def convert_path(srcpath: str, tgtpath: str, disk_mode: str, raw: bool, conv_mode: str,
-                 adj_mode: str, adj_actions: dict, osd: dict,
-                 trace_cmd: bool, cache_mode: str, numa_node: int, paral: int) -> str:
+# we need to create a pseudo disk for the virt install command to succeed (virt-install XXX)
+def translate_check_path(srcpath: str, tgtpath: str) -> str:
     os.makedirs(os.path.dirname(tgtpath), exist_ok=True)
-    if (disk_mode != "convert"):
-        # we need to create a pseudo disk for the virt install command to succeed
-        if (not os.path.exists(tgtpath)):
-            open(tgtpath, 'ab').close()
-        return tgtpath
+    if (not os.path.exists(tgtpath)):
+        open(tgtpath, 'ab').close()
+    return tgtpath
 
+
+def print_throughput(elapsed: float, path: str) -> None:
+    if (elapsed > 0.0):
+        targetstat = os.stat(path)
+        targetsize = targetstat.st_blocks * 512 // (1024 * 1024)
+        log.info("%s MiB in %s sec = %s MiB/s", targetsize, elapsed, targetsize // elapsed)
+
+
+def convert_path(srcpath: str, tgtpath: str, disk_mode: str, raw: bool, conv_mode: str,
+                 adj_mode: str, adj_actions: dict, macs: list, osd: dict,
+                 trace_cmd: bool, cache_mode: str, numa_node: int, paral: int) -> None:
     # CONVERSION / MOVE asked
     assert(disk_mode == "convert")
     if (srcpath.endswith(".vmdk")):
@@ -374,35 +382,35 @@ def convert_path(srcpath: str, tgtpath: str, disk_mode: str, raw: bool, conv_mod
             if (conv_mode == "v2v"):
                 conv_mode = "y"
 
+        stopwatch_start()
         if (conv_mode == "v2v"):
             img_v2v_convert(srcpath, tgtpath, trace_cmd, numa_node, raw)
         elif (conv_mode == "x"):
-            img_qemu_convert(srcpath, tgtpath, adj_mode, adj_actions, trace_cmd, cache_mode, numa_node, paral, raw)
+            img_qemu_convert(srcpath, tgtpath, adj_mode, adj_actions, macs, trace_cmd, cache_mode, numa_node, paral, raw)
         elif (conv_mode == "y"):
-            img_qemu_nbd_convert(srcpath, tgtpath, adj_mode, adj_actions, trace_cmd, cache_mode, numa_node, paral, raw)
+            img_qemu_nbd_convert(srcpath, tgtpath, adj_mode, adj_actions, macs, trace_cmd, cache_mode, numa_node, paral, raw)
         else:
             assert(0) # unhandled conv_mode value
+        print_throughput(stopwatch_elapsed(), tgtpath)
 
     elif (tgtpath != srcpath):
         try:
             if (filecmp.cmp(srcpath, tgtpath, shallow=True)):
                 log.info("disk already found at %s, no need to copy.", tgtpath)
-                return tgtpath
+                return
         except:
             log.info("could not compare to %s, assume we need to copy.", tgtpath)
 
         log.info("copying non-VMDK disk %s", tgtpath)
+        stopwatch_start()
         # use copy2 so we try to preserve modification time.
         shutil.copy2(srcpath, tgtpath)
+        print_throughput(stopwatch_elapsed(), tgtpath)
 
-    return tgtpath
 
-
+# run virt-install and return the generated XML as a string
 def virt_install(vinst_version: float,
-                 xml_name: str, fidelity: bool,
-                 disk_mode: str, raw: bool, skip_extra: bool, conv_mode: str,
-                 adj_mode: str, adj_actions: dict,
-                 trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int,
+                 xml_name: str, fidelity: bool, skip_extra: bool,
                  displayname: str, annotation: str,
                  cpu: dict, memory: int,
                  vcpus: int, sockets: int, cores: int, threads: int, vm_affinity: str,
@@ -412,7 +420,7 @@ def virt_install(vinst_version: float,
                  svga: bool, _svga_memory: int, vga: bool,
                  sound: str,
                  disk_ctrls: dict, disks: list, floppys: list,
-                 eths: list) -> None:
+                 eths: list) -> str:
     ### GENERAL SECTION - General Options for selecting the main functionality ###
     args: list = ["virt-install", "--print-xml", "--dry-run", "--noautoconsole", "--check", "all=off"]
     args.extend(["--virt-type", "kvm"])
@@ -525,18 +533,7 @@ def virt_install(vinst_version: float,
         if (skip_extra and not (disk["os"]["name"])):
             log.info("skipping extra non-OS disk %s", paths[0])
             continue
-        stopwatch_start()
-        path = convert_path(paths[0], paths[1], disk_mode, raw, conv_mode,
-                            adj_mode, adj_actions, disk["os"],
-                            trace_cmd, cache_mode, numa_node, parallel)
-        if (disk_mode == "convert"):
-            elapsed: float = stopwatch_elapsed()
-            if (elapsed > 0.0):
-                targetstat = os.stat(path)
-                targetsize = targetstat.st_blocks * 512 // (1024 * 1024)
-                log.info("%s MiB in %s sec = %s MiB/s",
-                         targetsize, elapsed, targetsize // elapsed)
-
+        path = translate_check_path(paths[0], paths[1])
         bus: str = disk["bus"]
         cache: str = disk["cache"]
         driver = disk["driver"]
@@ -591,16 +588,13 @@ def virt_install(vinst_version: float,
 
     log.debug("%s", args)
 
-    ### WRITE THE RESULTING DOMAIN XML ###
-    xml_file = open(xml_name, 'w', encoding="utf-8") if (xml_name) else sys.stdout
-    try:
-        subprocess.run(args, stdout=xml_file, check=True, encoding='utf-8')
-    except:
+    ### CREATE THE DOMAIN XML STRING ###
+    xml: str = runcmd(args, True)
+    if not (xml):
+        log.critical("virt-install did not return any output")
         log.critical(" ".join(args))
         sys.exit(1)
-
-    if (xml_name):
-        xml_file.close()
+    return xml
 
 
 # detect virt-install version only considering major.minor
@@ -1048,10 +1042,8 @@ def main(argc: int, argv: list) -> int:
     log.debug("%s", eths)
 
     # run virt-install to generate the xml
-    virt_install(vinst_version,
-                 xml_name, fidelity, disk_mode, raw, skip_extra, conv_mode,
-                 adj_mode, adj_actions,
-                 trace_cmd, cache_mode, numa_node, parallel,
+    xml: str = virt_install(vinst_version,
+                 xml_name, fidelity, skip_extra,
                  displayname, annotation,
                  cpu, memory,
                  vcpus, sockets, cores, threads, vm_affinity,
@@ -1062,6 +1054,27 @@ def main(argc: int, argv: list) -> int:
                  sound,
                  disk_ctrls, disks, floppys,
                  eths)
+
+    # extract macs from the xml
+    macs: list = re.findall(r"^.*mac address.*(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w).*$", xml, flags=re.MULTILINE)
+
+    if (disk_mode == "convert"):
+        for disk in disks:
+            paths: tuple = disk["path"]
+            convert_path(paths[0], paths[1], disk_mode, raw, conv_mode,
+                         adj_mode, adj_actions, macs, disk["os"],
+                         trace_cmd, cache_mode, numa_node, parallel)
+
+    ### WRITE THE RESULTING DOMAIN XML ###
+    # we need to do it here for v2v, since it writes a skeleton xml we need to overwrite
+    try:
+        xml_file = open(xml_name, 'w', encoding="utf-8") if (xml_name) else sys.stdout
+        xml_file.write(xml)
+        if (xml_name):
+            xml_file.close()
+    except:
+        log.critical("failed to write XML file %s", xml_name)
+        return 1
     return 0
 
 

testboot_xml.py
@@ -144,7 +144,7 @@ def modify_networks(domainname: str, network: str) -> None:
     virt_xml(domainname, ["--edit", "all", "--network", f"network={network}"])
 
 
-def overlay_adjust_disks(domainname: str, os_disks: list, adj_mode: str) -> list:
+def overlay_adjust_disks(domainname: str, os_disks: list, adj_mode: str, macs: list) -> list:
     overlays: list = []
     for disk in os_disks:
         (i, source) = disk
@@ -158,8 +158,8 @@ def overlay_adjust_disks(domainname: str, os_disks: list, adj_mode: str) -> list
         if (adj_mode != "none"):
             log.info("[ADJUST] %s", tmp.name)
             # we only need to inject the drivers here, no need to trim the image as we run it directly
-            adj_actions = {"drivers": True, "trim": False, "fstab": True}
-            adjust_guestfs(tmp.name, False, adj_mode, adj_actions)
+            adj_actions = {"drivers": True, "trim": False, "fstab": True, "net": True}
+            adjust_guestfs(tmp.name, False, adj_mode, adj_actions, macs)
         log.info("[DISK] REF %s", tmp.name)
         virt_xml(domainname, ["--edit", str(i + 1), "--disk", f"path={tmp.name}"])
         overlays.append(tmp)
@@ -285,15 +285,15 @@ def testboot_domain(domainname: str, adj_mode: str, timeout: int, layer2: bool, 
         log.critical("%s: no OS disks found, nothing to boot-test", domainname)
         sys.exit(1)
 
+    macs: list = find_macs(domainname)
+    modify_networks(domainname, sandbox)
+
     # XXX the overlays return value appears unused but there is a catch!
     # It needs to be here in this scope, so that the temporary files are not deleted before we run the test!
-    _ = overlay_adjust_disks(domainname, os_disks, adj_mode)
+    _ = overlay_adjust_disks(domainname, os_disks, adj_mode, macs)
 
     if (len(extra_disks) >= 1):
         remove_disks(domainname, extra_disks)
-
-    macs = find_macs(domainname)
-    modify_networks(domainname, sandbox)
 
     # start the domain.
     virsh(["start", domainname, "--paused"], True) # start paused to avoid race with is_zero

vmx2xml_mod/adjust/adjust.py
@@ -41,7 +41,7 @@ def adjust_guestfs_v2v(from_file: str) -> bool:
 
 
 # adjust using the experimental adjust_guestfs.py. Returns True on success.
-def adjust_guestfs_py(path: str, nbd: bool, adj_actions: dict) -> bool:
+def adjust_guestfs_py(path: str, nbd: bool, adj_actions: dict, macs: list) -> bool:
     args: list = ["adjust_guestfs.py", "-n" if (nbd) else "-f", path]
     if (adj_actions["drivers"]):
         args.append("-d")
@@ -49,6 +49,9 @@ def adjust_guestfs_py(path: str, nbd: bool, adj_actions: dict) -> bool:
         args.append("-t")
     if (adj_actions["fstab"]):
         args.append("-s")
+    if (adj_actions["net"]):
+        for mac in macs:
+            args.extend(["-m", mac])
 
     v: int; q: int
     (v, q) = log_get_vq()
@@ -64,7 +67,7 @@ def adjust_guestfs_py(path: str, nbd: bool, adj_actions: dict) -> bool:
     return True
 
 
-def adjust_guestfs(path: str, nbd: bool, adj_mode: str, adj_actions: dict) -> bool:
+def adjust_guestfs(path: str, nbd: bool, adj_mode: str, adj_actions: dict, macs: list) -> bool:
     if (adj_mode == "none"):
         log.warning('adjust_guestfs: unexpected call with adjustment mode "none"')
         return False
@@ -73,7 +76,7 @@ def adjust_guestfs(path: str, nbd: bool, adj_mode: str, adj_actions: dict) -> bo
     if (adj_mode == "v2v"):
         rv = adjust_guestfs_v2v(path)
     elif (adj_mode == "x"):
-        rv = adjust_guestfs_py(path, nbd, adj_actions)
+        rv = adjust_guestfs_py(path, nbd, adj_actions, macs)
     else:
         assert(0)               # unsupported adj_mode
 

vmx2xml_mod/img/img.py
@@ -136,12 +136,12 @@ def img_qemu_info(from_file: str) -> int:
     return int(vsize_m.group(1))
 
 
-def img_qemu_convert(sourcepath: str, targetpath: str, adj_mode: str, adj_actions: dict,
+def img_qemu_convert(sourcepath: str, targetpath: str, adj_mode: str, adj_actions: dict, macs: list,
                      trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int, raw: bool) -> None:
     src: str = sourcepath
     if (adj_mode != "none"):
         tmp = img_qemu_create_overlay(sourcepath, "vmdk")
-        adjust_guestfs(tmp.name, False, adj_mode, adj_actions)
+        adjust_guestfs(tmp.name, False, adj_mode, adj_actions, macs)
         src = tmp.name
 
     img_qemu_copy(src, targetpath, trace_cmd, cache_mode, numa_node, parallel, raw)
@@ -192,11 +192,11 @@ def img_qemu_nbd_copy(sin: str, sout: str, trace_cmd: bool, numa_node: int, para
         img_wait_child(tpid)
 
 
-def img_qemu_nbd_convert(sourcepath: str, targetpath: str, adj_mode: str, adj_actions: dict,
+def img_qemu_nbd_convert(sourcepath: str, targetpath: str, adj_mode: str, adj_actions: dict, macs: list,
                          trace_cmd: bool, cache_mode: str, numa_node: int, parallel: int, raw: bool) -> None:
     (sin, pidin) = img_qemu_nbd_create(sourcepath, adj_mode != "none", cache_mode, False, adj_mode == "none")
     if (adj_mode != "none"):
-        adjust_guestfs(sin.name, True, adj_mode, adj_actions)
+        adjust_guestfs(sin.name, True, adj_mode, adj_actions, macs)
     vsize: int = img_qemu_info(sourcepath)
     img_qemu_create(targetpath, vsize, raw)
     (sout, pidout) = img_qemu_nbd_create(targetpath, False, cache_mode, raw, False)

commit ce6e016fb95024ea80e5cc2a708129c28a570ae0

vmx2xml.py: main (convert): skip extra disks that have not been translated
a bug has been introduced in commit: 2bb6313de1507b8369c9d3851a14ea406025a825
("vmx2xml.py: refactor writing of XML to make --adjust-networks effective")

when the path translation step has been separated from the image conversion.

Before that commit, both translation and conversion would be skipped for
non-OS extra_disks, but now the convert loop includes non-OS extra_disks.

Restore the situation by explicitly skipping them in conversion too.

Fixes: 2bb6313de1507b8369c9d3851a14ea406025a825
Signed-off-by: Claudio Fontana <cfontana@suse.de>

vmx2xml.py
@@ -532,7 +532,7 @@ def virt_install(vinst_version: float,
         device = disk["device"]
         paths: tuple = disk["path"]
         if (skip_extra and not (disk["os"]["name"])):
-            log.info("skipping extra non-OS disk %s", paths[0])
+            log.info("virt_install: skipping extra non-OS disk %s", paths[0])
             continue
         path = translate_check_path(paths[1])
         bus: str = disk["bus"]
@@ -1060,6 +1060,9 @@ def main(argc: int, argv: list) -> int:
     if (disk_mode == "convert"):
         for disk in disks:
             paths: tuple = disk["path"]
+            if (skip_extra and not (disk["os"]["name"])):
+                log.info("convert: skipping extra non-OS disk %s", paths[0])
+                continue
             convert_path(paths[0], paths[1], disk_mode, raw, conv_mode,
                          adj_mode, adj_actions, macs, disk["os"],
                          trace_cmd, cache_mode, numa_node, parallel)